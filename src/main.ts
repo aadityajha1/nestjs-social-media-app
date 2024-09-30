bb5ca5e941af844a0969a2dd83c2eaeff7a11664
Aaditya Jha <aadityajha2000@gmail.com>
2024-09-30 21:21:33 +0545

    fix: fixed csrf issue for uploading files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+// Ignore the import error
+// @ts-ignore
+import { graphqlUploadExpress } from 'graphql-upload-ts';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'x-apollo-operation-name'],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
