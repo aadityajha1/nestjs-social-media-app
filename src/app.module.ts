679c8fe9073285caa9d517bd4abe8c1126ed0705
Aaditya Jha <aadityajha2000@gmail.com>
2024-09-30 15:22:36 +0545

    feat: configured mongoose and graphql in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'schema.gql'), // Generates schema automatically in memory
+      // plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      playground: true, // Disables GraphQL Playground
+      introspection: true, // Enables GraphQL Introspection
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    UsersModule,
+    AuthModule,
+    PostsModule,
+    CommentsModule,
+    // GraphqlModule,
+  ],
+
+  controllers: [],
+  providers: [AppResolver, AppService],
 })
 export class AppModule {}
